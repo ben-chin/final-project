836818136a2d3a495271e031a495da668055f921
ben-chin <ben.chin101@gmail.com>
2016-05-03 05:48:39 +0100

    Add serialize-on-load categorizer to use in views (for now)

--- project/web/views.py
+++ project/web/views.py
@@ -3,6 +3,17 @@ from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
 
 
+# TODO: refactor this to somewhere else
+import os
+from project.settings import BASE_DIR
+from autocat import ACSerializer
+
+PATH_TO_AC_PKL = os.path.join(BASE_DIR, 'data', 'ac', 'categorizer.pkl')
+print 'loading cat'
+ac = ACSerializer.load(PATH_TO_AC_PKL)
+print 'fully loaded cat'
+
+
 def index(request):
     return render(request, 'web/index.html')
 
